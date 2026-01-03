357758160a8c0e1cbec7b87c528835337aa05210
MWAQASALI2 <itsmwaqasali@gmail.com>
2026-01-04 02:59:44 +0500

    Final

---
+++ code.py
@@ -0,0 +1,19 @@
+import requests
+token="17361~ZuZMwtAMYFYv3QRJkePnDtNrEM6DGV6e3YaG3WMhGyTvvvaWR3tY2wvFeCBGWMUD"
+headers={
+    "Authorization":f"Bearer {token}"
+}
+response=requests.get("https://hulms.instructure.com/api/v1/users/self/enrollments",headers=headers)
+
+json=response.json()
+dict={}
+for i in json:
+    dict[i['course_id']]=i['grades']['current_score']
+scores={}
+for id in dict:
+    course_response=requests.get(f"https://hulms.instructure.com/api/v1/courses/{id}",headers=headers)
+    course_response=course_response.json()
+    scores[course_response["name"]]=dict[id]
+for i in scores:
+    if scores[i]!=None:
+        print(f"Your grade in {i} is {scores[i]}%")
