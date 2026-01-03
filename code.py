f0ee8074a1a7a421f26f0eeb957e272785922025
MWAQASALI2 <itsmwaqasali@gmail.com>
2026-01-04 03:09:04 +0500

    Final changes

--- code.py
+++ code.py
@@ -10,10 +10,12 @@ dict={}
 for i in json:
     dict[i['course_id']]=i['grades']['current_score']
 scores={}
+
 for id in dict:
     course_response=requests.get(f"https://hulms.instructure.com/api/v1/courses/{id}",headers=headers)
     course_response=course_response.json()
     scores[course_response["name"]]=dict[id]
+    
 for i in scores:
     if scores[i]!=None:
         print(f"Your grade in {i} is {scores[i]}%")
